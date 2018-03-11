14b7db4271f59a61487195090a716163c3f685d0
Klemen Rahne <rahne.klemen@gmail.com>
2018-03-11 20:22:41 +0100

    Initial commit.

---
+++ Src/main.c
@@ -0,0 +1,449 @@
+/**
+  ******************************************************************************
+  * File Name          : main.c
+  * Description        : Main program body
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2017 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32l1xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim2;
+
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+int stevec=0;
+int cas=0;
+float hitrost;
+char data_tx[10];
+char data_rx[2];
+int len;
+int overflow=0;
+float obseg=1.0;
+int obseg_int=1;
+float razdalja;
+char* stevka_tmp;
+char lcd_text[6];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void Error_Handler(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM2_Init(void);
+static void MX_USART1_UART_Init(void);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+char *substring(int i,int j,char *ch)
+{
+    int n,k=0;
+    char *ch1;
+    ch1=(char*)malloc((j-i+1)*1);
+    n=j-i+1;
+
+    while(k<n)
+    {
+        ch1[k]=ch[i];
+        i++;k++;
+    }
+
+    return (char *)ch1;
+}
+
+float distance(int counter, float circum){
+  return (float)(counter * circum);
+}
+
+float speed(float time, float circum){
+  return (float)(circum/time);
+}
+
+
+//ineruptna rutina za 'magnetni' stik na A5-GND
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+  if (overflow!=0){
+    TIM2->CNT=0;
+    stevec++;
+    overflow=0;
+  }
+  else{
+    cas = TIM2->CNT;
+    stevec++;
+/*
+    if (stevec<50){
+      sprintf(lcd_text, "%d",stevec);
+      BSP_LCD_GLASS_Clear();
+      BSP_LCD_GLASS_DisplayString((uint8_t*)lcd_text);
+    }
+    if (stevec % 5 == 0 && stevec >= 50 ){
+      povrsina=stevec/(float)(4100);
+      sprintf(lcd_text, "%02.3f",povrsina);
+      BSP_LCD_GLASS_Clear();
+      BSP_LCD_GLASS_DisplayString((uint8_t*)lcd_text);
+    }*/
+    if (cas==0){
+      hitrost=0.0;
+    }
+    else{
+      //hitrost=obseg/10/(float)cas;
+      hitrost=speed((float)cas, obseg);
+    }
+    if (hitrost==0.0){
+        sprintf(data_tx, "#%.3f-0~",distance(stevec,obseg));
+        len=strlen(data_tx);
+        HAL_UART_Transmit( &huart1, data_tx, len, 100);
+    }
+    else{
+          sprintf(data_tx, "#%.3f-%2.1f~",distance(stevec,obseg), hitrost);
+          len=strlen(data_tx);
+          HAL_UART_Transmit( &huart1, data_tx, len, 100);
+    }
+    TIM2->CNT=0;
+  }
+}
+
+
+
+
+  /**
+* @brief This function handles TIM2 global interrupt.
+*/
+void TIM2_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM2_IRQn 0 */
+  
+  TIM2->SR &=~TIM_SR_UIF;
+  overflow++;
+  if (overflow==1){ 
+    sprintf(data_tx, "#%.3f-0~",distance(stevec, obseg));
+    len=strlen(data_tx);
+    HAL_UART_Transmit( &huart1, (uint8_t*)data_tx, len, 100);
+  }
+  
+  /* USER CODE END TIM2_IRQn 0 */
+  //HAL_TIM_IRQHandler(&htim2);
+  /* USER CODE BEGIN TIM2_IRQn 1 */
+ 
+  /* USER CODE END TIM2_IRQn 1 */
+}
+
+//variables need to be declared at the beginning for USART
+char Rx_indx, Rx_data[6], Rx_Buffer[20], Transfer_cplt;
+int vred;
+//Interrupt callback routine for USART receive
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	uint8_t i;
+	if (huart->Instance == USART1)	//current UART
+		{
+		if (Rx_indx==0) {for (i=0;i<20;i++) Rx_Buffer[i]=0;}	//clear Rx_Buffer before receiving new data
+                  if (Rx_data[0]!=33)	//if received data different from ascii 33 (!)
+                          {
+                          Rx_Buffer[Rx_indx++]=Rx_data[0];	//add data to Rx_Buffer
+                          
+                          }
+                  else			//if received data = 13
+                          {
+                          Rx_indx=0;
+                          Transfer_cplt=1;//transfer complete, data is ready to read
+                          if (Rx_Buffer[0]==35) //35=#=nastavljanje dolzine
+                          {
+                            stevka_tmp=substring(1,sizeof(Rx_Buffer),Rx_Buffer);
+                            stevec=atoi(stevka_tmp);
+                            sprintf(data_tx, "-%d~",stevec);
+                            len=strlen(data_tx);
+                            HAL_UART_Transmit( &huart1, (uint8_t*)data_tx , len, 100);
+                            }
+                          if (Rx_Buffer[0]==37) //37=%=nastavljanje obsega
+                          {
+                            
+                            stevka_tmp=substring(1,sizeof(Rx_Buffer),Rx_Buffer);
+                            obseg_int=atoi(stevka_tmp);
+                            sprintf(data_tx, "+%d~",obseg_int);
+                            len=strlen(data_tx);
+                            obseg=(float)obseg_int/1000;
+                            HAL_UART_Transmit( &huart1, (uint8_t*)data_tx , len, 100);
+                            }
+                          if (Rx_Buffer[0]==36) //36=$=refresh button
+                          {
+                        	  float debug=distance(stevec, obseg);
+                            sprintf(data_tx, "#%.3f-0~", distance(stevec, obseg));
+                            len=strlen(data_tx);
+                            HAL_UART_Transmit( &huart1, (uint8_t*)data_tx , len, 100);
+                            }
+                          }
+                  HAL_UART_Receive_IT(&huart1, Rx_data, 1);	//activate UART receive interrupt every time
+		}
+
+ 
+}
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  //A5-GND
+  MX_GPIO_Init();
+  
+  MX_TIM2_Init();
+  /* pomen pinov na plosci:
+  TX:B6
+  RX:B7
+  povezava na companion device:
+  tx->B7
+  rx->B6
+  */  
+  MX_USART1_UART_Init();
+
+  BSP_LCD_GLASS_Init();
+
+  /* USER CODE BEGIN 2 */
+  HAL_TIM_Base_Start_IT(&htim2); //zaceek interupta za timer  
+  TIM2->CR1 |= 0x0001;
+  
+  HAL_UART_Receive_IT(&huart1, (uint8_t*)Rx_data, 1); //start inerupta za uart
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+   while (1)
+  {
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+
+}
+
+/** System Clock Configuration
+*/
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+    /**Configure the main internal regulator output voltage 
+    */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = 0;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_5;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+/* TIM2 init function */
+static void MX_TIM2_Init(void)
+{
+
+  TIM_ClockConfigTypeDef sClockSourceConfig;
+  TIM_MasterConfigTypeDef sMasterConfig;
+
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 2097;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 999; //stevilo sekund (ms!!!!), da se hitrost spravi na 0, po ustavitvi
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+/* USART1 init function */
+static void MX_USART1_UART_Init(void)
+{
+
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin : PA5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1) 
+  {
+  }
+  /* USER CODE END Error_Handler */ 
+}
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
